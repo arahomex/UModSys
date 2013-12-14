86413fdfffe44be56e53779c7929aca25cc39dff
rewoonenco <none@none>
2013-12-14 14:28:54 +0000

    =
    --HG--
    extra : convert_revision : svn%3Ac7b0911a-351f-11e0-b982-4926ce03a894%40545

---
+++ build/gnu-make/generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bp=`realpath $0`
+bp=`dirname $bp`
+
+cd $bp
+echo ************************ Generate build files...
+perl ../_gen/gen_build.pl generator.txt
+
+echo ************************ Generate version files...
+cd $bp/../..
+if [ ! -d $bp/Versions ] ; then
+  mkdir $bp/Versions
+  rm $bp/Versions/*.sh
+fi
+perl build/_gen/update_versions.pl --file:build/_gen/versions.txt --genpath:$bp/Versions --gentype:build/_gen/version-generator-sh.txt --set:platform:platform/linux
+perl build/_gen/update_versions.pl --file:build/_gen/versions.txt --set:platform:platform/linux --mode:auto --all
+
+echo ************************ Done
+
