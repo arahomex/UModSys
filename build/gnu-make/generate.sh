de94a4920abdd46aff2a8033aa4986a50ae47cdf
rewoonenco <none@none>
2015-07-27 13:53:35 +0000

    --HG-- extra : convert_revision : svn%3Ac7b0911a-351f-11e0-b982-4926ce03a894%40886

--- build/gnu-make/generate.sh
+++ build/gnu-make/generate.sh
@@ -7,7 +7,7 @@ cd $bp
 echo '************************ Generate build files...'
 export PLATFORM="`uname`_`uname -p`"
 export PLATFORMS="`uname`_`uname -p`"
-perl ../_gen/gen_build.pl generator.txt
+perl ../_generator/gen_build.pl generator.txt
 exit
 echo '************************ Generate version files...'
 cd $bp/../..
@@ -15,8 +15,8 @@ if [ ! -d $bp/Versions ] ; then
   mkdir $bp/Versions
   rm $bp/Versions/*.sh
 fi
-perl build/_gen/update_versions.pl --file:build/_gen/versions.txt --genpath:$bp/Versions --gentype:build/_gen/version-generator-sh.txt --set:platform:platform/linux
-perl build/_gen/update_versions.pl --file:build/_gen/versions.txt --set:platform:platform/linux --mode:auto --all
+perl build/_generator/update_versions.pl --file:build/_gen_rules/versions.txt --genpath:$bp/Versions --gentype:build/_gen/version-generator-sh.txt --set:platform:platform/linux
+perl build/_generator/update_versions.pl --file:build/_gen_rules/versions.txt --set:platform:platform/linux --mode:auto --all
 
 echo '************************ Done'
 
